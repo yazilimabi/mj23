1315b5852f1da90fa27a0a40111244c2fbdf2aa0
Kenpar <erengrl3854@gmail.com>
2023-11-18 17:36:48 +0300

    lever fix

--- Assets/Scripts/Electronics/Lever.cs
+++ Assets/Scripts/Electronics/Lever.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Lever : PowerGenerator
 {
     [SerializeField] SpriteRenderer spriteRenderer;
-
+    [SerializeField] float interactTimer = 0.75f;
+    float timer = 0f;
     public override void UpdateState(){
         if(state){
             spriteRenderer.color = Color.green;
         }else{
             spriteRenderer.color = Color.red;
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player") && timer <= 0){
+            SetState(!state);
+            timer = interactTimer;
+        }
+    }
+    
+    void FixedUpdate(){
+        if(timer > 0) timer-=Time.fixedDeltaTime;
     }
 }
