db3fedb9457a58a89fb1bdb5e0397b524f791373
tixvage <mel10h3421@gmail.com>
2023-11-18 22:46:41 +0300

    player health

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] float enemyDamage = 50f;
+    [SerializeField] float playerDamage = 50f;
+
     void OnCollisionEnter2D(Collision2D col){
         Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D col){
         if(col.CompareTag("Player")){
+            col.transform.parent.GetComponent<PlayerHealth>().Damage(playerDamage);
             Destroy(gameObject);
         } else if (col.CompareTag("Enemy")) {
-            col.transform.parent.GetComponent<EnemyHealth>().Damage(100);
+            col.transform.parent.GetComponent<EnemyHealth>().Damage(enemyDamage);
+            Destroy(gameObject);
         }
         else if(col.CompareTag("Lever")){
             col.GetComponent<PowerGenerator>().SetState(!col.GetComponent<PowerGenerator>().state);
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] UnityEvent onDeath;
+    [SerializeField] float maxHealth = 100f;
+    float health;
+
+    void Start() {
+        health = maxHealth;
+    }
+
+    public void Recover() {
+        health = maxHealth;
+    }
+
+    public void Damage(float damage) {
+        if(health <= 0f)
+            return;
+        health -= damage;
+        if(health <= 0f){
+            onDeath.Invoke();
+        }
+    }
+}
