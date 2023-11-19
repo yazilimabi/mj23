a4f5f9fb7154b6c8873aa4615acdf74596e0af46
MFaik <m.faikekclr@gmail.com>
2023-11-19 18:43:39 +0300

    elevator fixi

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -10,13 +10,13 @@ public class Elevator : MonoBehaviour
     [SerializeField] GameObject guvenlikMesaji;
 
     void Update() {
-        if(powerGenerator.state||true){
+        if(powerGenerator.state){
             guvenlikMesaji.SetActive(false);
         }
     }
 
     void OnTriggerEnter2D(Collider2D col){
-        //if(powerGenerator.state)
+        if(powerGenerator.state)
             if(col.CompareTag("Player")){
                 ChangeScene();
             }
